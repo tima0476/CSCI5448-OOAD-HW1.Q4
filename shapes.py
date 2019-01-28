b709478e5a06ff53161c2101971432d91c1d147f
Tim Mason <Timothy.Mason@Colorado.edu>
2019-01-27 23:04:30 -0700

    Create shapes.py
    Nothing here yet except for a header

---
+++ shapes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+	print("University of Colorado at Boulder")
+	print("CSCI 5448 - Object Oriented Analysis And Design")
+	print("")
+	print("Solution to Homework 1, Question 4")
+	print("name: Timothy Mason")
+	print("collaborators: none")
