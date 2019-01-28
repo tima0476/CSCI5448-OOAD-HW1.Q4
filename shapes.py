3a673ecd805176070fadc62ed1cb6def9fcb9087
Tim Mason <Timothy.Mason@Colorado.edu>
2019-01-27 23:25:41 -0700

    Update shapes.py
    Added comments to start fleshing out the structure of the program.

--- shapes.py
+++ shapes.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+#
+# CSCI 5448 - OOAD - Homework 1, Question 4 solution
+# name: Timothy Mason;   Collaborators: none
+
+# Class definition for Shapes base class
+
+# Class definition for Circle subclass
+
+# Class definition for Square subclass
+
+# Class definition for Triangle subclass
+
 
 if __name__ == "__main__":
 	print("University of Colorado at Boulder")
@@ -7,3 +19,9 @@ if __name__ == "__main__":
 	print("Solution to Homework 1, Question 4")
 	print("name: Timothy Mason")
 	print("collaborators: none")
+
+	# load the "database"
+
+	# sort the data
+
+	# "draw" the shapes
